aec5c18af235c8c9340bff140eca80e6b74d1028
felipe <felipereynoso171@gmail.com>
2025-03-29 03:10:01 +0000

    Cambios de nuevo

--- eya1/programacion/practica0/ejer6.c
+++ eya1/programacion/practica0/ejer6.c
@@ -1,17 +1,19 @@
+/*
+Implemente una función char *get_new_line(void) que ingrese una linea por teclado (hasta \n), y
+devuelva un puntero a la cadena ingresada. 
+*/
+
 #include<stdio.h>
 #include<stdlib.h>
-
 char *get_new_line(){
-    // char palabra;
-    char *pc = malloc(sizeof(char));
-    printf("Ingrese una linea por teclado hasta apretar enter: \n");
-    scanf("%c", pc);
-    // pc = palabra;
-    free(pc);
-    return pc; 
+    
+    
+    
 }
 
 
 int main(){
+
+    
     return 0;
 }
