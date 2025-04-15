95c6c479f026b45fbd8358a423e216d04671bc03
Felipe <felipereynoso171@gmail.com>
2025-04-15 11:16:19 -0300

    Practica 2

---
+++ practica2/slist/slist.c
@@ -0,0 +1,69 @@
+#include "slist.h"
+#include <stdlib.h>
+
+SList slist_crear() {
+  return NULL;
+}
+
+void slist_destruir(SList lista) {
+  SNodo *nodoAEliminar;
+  while (lista != NULL) {
+    nodoAEliminar = lista;
+    lista = lista->sig;
+    free(nodoAEliminar);
+  }
+}
+
+int slist_vacia(SList lista) {
+  return lista == NULL;
+}
+
+SList slist_agregar_final(SList lista, int dato) {
+  SNodo *nuevoNodo = malloc(sizeof(SNodo));
+  nuevoNodo->dato = dato;
+  nuevoNodo->sig = NULL;
+
+  if (lista == NULL)
+    return nuevoNodo;
+
+  SList nodo = lista;
+  for (;nodo->sig != NULL;nodo = nodo->sig);
+  /* ahora 'nodo' apunta al ultimo elemento en la lista */
+
+  nodo->sig = nuevoNodo;
+  return lista;
+}
+
+SList slist_agregar_inicio(SList lista, int dato) {
+  SNodo *nuevoNodo = malloc(sizeof(SNodo));
+  nuevoNodo->dato = dato;
+  nuevoNodo->sig = lista;
+  return nuevoNodo;
+}
+
+void slist_recorrer(SList lista, FuncionVisitante visit) {
+  for (SNodo *nodo = lista; nodo != NULL; nodo = nodo->sig)
+    visit(nodo->dato);
+}
+
+int slist_longitud(SList lista){
+  if(lista == NULL){ return 0; }
+  int i = 0;
+  for(SNodo *temp = lista; temp != NULL; temp = temp->sig){
+    i++;
+  }
+  return i;
+}
+
+
+SList slist_concatenar(SList lista1, SList lista2){
+  if(lista1 == NULL || lista2 == NULL){
+    return NULL;
+  }
+
+  for(SNodo *temp = lista1; temp != NULL; temp = temp->sig);
+
+  
+}
+
+
